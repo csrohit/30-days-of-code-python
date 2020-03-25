f2b9b77b09e02e5cdb9cc3d64a8d7cf937cfc50a
Rohit Nimkar <nehalnimkar@gmail.com>
2020-03-25 09:55:09 +0530

    added day28.py

---
+++ day28.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    op = []
+
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+
+        firstName = firstNameEmailID[0]
+
+        emailID = firstNameEmailID[1]
+        
+        if re.search('.+@gmail\.com$', emailID):
+            op.append(firstName)
+    
+    op.sort()
+    print(*op, sep = '\n')
